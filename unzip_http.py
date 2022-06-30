7490d8660807bd5298f20a11fb5650bb79cceabd
Saul Pwanson <code@saul.pw>
2022-06-29 23:20:37 -0700

    Support ZIP64 #4

--- unzip_http.py
+++ unzip_http.py
@@ -23,11 +23,36 @@ class RemoteZipInfo:
     compress_size:int = 0
     file_size:int = 0
 
+    def parse_extra(self, extra):
+        i = 0
+        while i < len(extra):
+            fieldid, fieldsz = struct.unpack_from('<HH', extra, i)
+            i += 4
+
+            if fieldid == 0x0001:  # ZIP64
+                if fieldsz == 8: fmt = '<Q'
+                elif fieldsz == 16: fmt = '<QQ'
+                elif fieldsz == 24: fmt = '<QQQ'
+                elif fieldsz == 28: fmt = '<QQQI'
+
+                vals = list(struct.unpack_from(fmt, extra, i))
+                if self.file_size == 0xffffffff:
+                    self.file_size = vals.pop(0)
+
+                if self.compress_size == 0xffffffff:
+                    self.compress_size = vals.pop(0)
+
+                if self.header_offset == 0xffffffff:
+                    self.header_offset = vals.pop(0)
+
+            i += fieldsz
+
 
 class RemoteZipFile:
-    fmt_endcdir = 'IHHHHIIH'
-    fmt_cdirentry = '<IHHHHIIIIHHHHHII'
-    fmt_localhdr = '<IHHHIIIIHH'
+    fmt_eocd = '<IHHHHIIH'  # end of central directory
+    fmt_eocd64 = '<IQHHIIQQQQ'  # end of central directory ZIP64
+    fmt_cdirentry = '<IHHHHIIIIHHHHHII'  # central directory entry
+    fmt_localhdr = '<IHHHIIIIHH'  # local directory header
 
     def __init__(self, url):
         self.url = url
@@ -45,9 +70,23 @@ class RemoteZipFile:
 
         sz = int(resp.headers['Content-Length'])
         resp = self.get_range(sz-65536, 65536)
-        i = resp.data.rfind(b'\x50\x4b\x05\x06')
 
-        magic, disk_num, disk_start, disk_num_records, total_num_records, cdir_bytes, cdir_start, comment_len = struct.unpack_from(self.fmt_endcdir, resp.data, offset=i)
+        eocdloc = eocd = eocd64 = None
+
+        cdir_start = 0
+        i = resp.data.rfind(b'\x50\x4b\x06\x06')
+        if i >= 0:
+            magic, eocd_sz, create_ver, min_ver, disk_num, disk_start, disk_num_records, total_num_records, \
+                cdir_bytes, cdir_start = struct.unpack_from(self.fmt_eocd64, resp.data, offset=i)
+        else:
+            i = resp.data.rfind(b'\x50\x4b\x05\x06')
+            if i >= 0:
+                magic, \
+                    disk_num, disk_start, disk_num_records, total_num_records, \
+                    cdir_bytes, cdir_start, comment_len = struct.unpack_from(self.fmt_eocd, resp.data, offset=i)
+
+        if cdir_start <= 0 or cdir_start > sz:
+            error('cannot find central directory')
 
         filehdr_index = 65536 - (sz - cdir_start)
         cdir_end = filehdr_index + cdir_bytes
@@ -59,11 +98,21 @@ class RemoteZipFile:
                 disknum_start, internal_attr, external_attr, local_header_ofs = \
                     struct.unpack_from(self.fmt_cdirentry, resp.data, offset=filehdr_index)
 
-            filename = resp.data[filehdr_index+sizeof_cdirentry:filehdr_index+sizeof_cdirentry+fnlen]
+            filehdr_index += sizeof_cdirentry
 
-            filehdr_index += sizeof_cdirentry + fnlen + extralen + commentlen
+            filename = resp.data[filehdr_index:filehdr_index+fnlen]
+            filehdr_index += fnlen
 
-            yield RemoteZipInfo(filename.decode(), date_time, local_header_ofs, method, complen, uncomplen)
+            extra = resp.data[filehdr_index:filehdr_index+extralen]
+            filehdr_index += extralen
+
+            comment = resp.data[filehdr_index:filehdr_index+commentlen]
+            filehdr_index += commentlen
+
+            rzi = RemoteZipInfo(filename.decode(), date_time, local_header_ofs, method, complen, uncomplen)
+
+            rzi.parse_extra(extra)
+            yield rzi
 
     def get_range(self, start, n):
         return self.http.request('GET', self.url, headers={'Range': f'bytes={start}-{start+n-1}'}, preload_content=False)
