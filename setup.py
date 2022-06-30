3223dc13db1d00a3952624a6f4cf97b21561d030
anjakefala <anja.kefala@gmail.com>
2022-06-29 23:57:14 -0700

    [build] bump to v0.2

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ def requirements():
 
 setup(
         name="unzip-http",
-        version="0.1",
+        version="0.2",
         description="extract files from .zip files over http without downloading entire archive",
         long_description=readme(),
         long_description_content_type="text/markdown",
